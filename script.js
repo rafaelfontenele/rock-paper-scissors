92ae7a1213302edefda35322d08683ad95748b02
Etzoider <98551688+etzoider@users.noreply.github.com>
2022-12-10 14:32:13 -0300

    Updating script and css

--- script.js
+++ script.js
@@ -9,13 +9,24 @@ function isPossible(move) {
 }   
 
 function battleResult(hand1, hand2) {
+/*
+input: (str, str)
+returns: -1 == something went wrong(invalid move)
+          0 == DRAW
+          1 == Hand1 won
+          2 == Hand2 won
+*/
+
     let result;
+
     if (typeof hand1 !== 'string' || typeof hand2 != 'string') {
         return -1
     }
 
     hand1 = hand1.toLowerCase();
     hand2 = hand2.toLowerCase();
+
+    result = -1
 
     if (possibleMoves.indexOf(hand1) == -1 || possibleMoves.indexOf(hand1) == -1) {
         return -1
@@ -27,20 +38,20 @@ function battleResult(hand1, hand2) {
     } else if (hand1 == 'rock') {
         if (hand2 == 'scissors') {
             result = 1
-        } else {
+        } else if (hand2 == 'paper') {
             result = 2
         } 
     } else if (hand1 == 'paper') {
         if (hand2 == 'rock') {
             result = 1;
-        } else {
+        } else if (hand2 == 'scissors') {
             result = 2;
         }
 
     } else if (hand1 == 'scissors') {
         if (hand2 == 'paper') {
             result = 1;
-        } else {
+        } else if (hand2 == 'rock') {
             result = 2;
         }
     } 
@@ -57,13 +68,6 @@ function randomMove() {
     return possibleMoves[makeRandomChoice()];
 }
 
-function startGame() {
-    while (true) {
-        let player = prompt("\nChoose wisely:\n\n-rock\n-paper\n-scissors\n");
-        if (!isPossible(player)) {
-            console.log("You didn't choose wisely, try again.");
-        } else {
-            console.log(`You chose ${player}`);
-        }
-    }
+function playerMove(move) {
+    console.log(`Player played ${move}`)
 }
